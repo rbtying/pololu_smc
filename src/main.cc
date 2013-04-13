67bbe87808a65d5313a31e5f68236182d21aa1eb
Robert Ying <rbtying@aeturnalus.com>
2013-04-13 15:54:50 -0400

    modified to make Pololu_SMC

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,4 @@
 #include <ros/ros.h>
-#include "ParallaxBase.h"
 
 /**
  * Main runner function for the node
@@ -7,6 +6,5 @@
 int main(int argc, char** argv) {
     // init ros
     ros::init(argc, argv, "parallax_base");
-    ParallaxBase base;
     ros::spin();
 }
